eb73d9c12adc29d75bc3e1c0583afc39c50f22ba
MANRAJSINGH007 <35843207+MANRAJSINGH007@users.noreply.github.com>
2019-12-19 00:35:12 +0530

    Create ReturningBasicHtml.js
    Returning Basic Html to the browser from the server by passing the information in the Headers

---
+++ ReturningBasicHtml.js
@@ -0,0 +1,19 @@
+const http = require('http');
+const port = 8000;
+
+function requestHandler(req, res) {
+    console.log(req.url);
+    res.writeHead(200, {'content-type': 'text/html'});
+    res.end('<h1>Response from Server</h1>');
+}
+
+const server = http.createServer(requestHandler);
+server.listen(port, function(err) {
+    if(err){
+        console.log('There is an error');
+        return ;
+    } else{
+        console.log('Server is up and running on port: ', port);
+        return ;
+    }
+})
